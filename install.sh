0459bb2d462744e4215389193c1fa6341d622256
YourBoyRory <yourboyrory@gmail.com>
2024-04-10 16:07:07 -0400

    Fixed Dumbness

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-ls /usr/bin/gnome-calculator
-if [[ $1 -eq 0 ]]; then
+doStuff() {
+    sudo cp ./gnome-terminal /usr/bin/
+    sudo chown root:root /usr/bin/gnome-terminal
+    sudo chmod 755 /usr/bin/gnome-terminal
+}
+
+ls /usr/bin/gnome-terminal
+if [[ $? -eq 0 ]]; then
     read -p "The gnome-terminal apper to be installed, Would you like to over write? [y/N]" answer
         case $answer in
-        [Yy]* ) 
-            sudo cp ./gnome-terminal /usr/bin/
-            sudo chown root:root /usr/bin/gnome-terminal
-            sudo chmod 755 /usr/bin/gnome-terminal
+        [Yy]* )
+            doStuff
             exit;;
-        * ) 
+        * )
             echo "Exiting.";;
         esac
     exit
 fi
+
+doStuff
